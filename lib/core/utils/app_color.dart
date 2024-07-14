18d37e7bf00116e8f6145792e49b51e9c6b065c2
ahmed tarek <ahmedtarekat2338@gmail.com>
2024-07-14 21:36:58 +0300

    handle app colors with theme

--- lib/core/utils/app_color.dart
+++ lib/core/utils/app_color.dart
@@ -9,7 +9,7 @@ class AppColors {
   static Color dark =  const Color(0xff333739);
   static Color black =  Colors.black;
   static Color grey=Colors.grey;
-  static Color red=Colors.red;
+  static Color red=Colors.red[900]!;
   static Color deepPurple=Colors.deepPurple[900]!;
 }
 
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:otp_creative_minds/core/utils/app_color.dart';
 
 abstract class Styles {
   static final textStyle20Red = TextStyle(
       fontSize: 20,
-      color: Colors.red[900]
+      color: AppColors.red
   );
-  static final textStyle15Grey = TextStyle(fontSize: 15, color: Colors.grey[600]);
-  static const textStyle15Black =  TextStyle(
+  static final textStyle15Grey = TextStyle(fontSize: 15, color: AppColors.grey);
+  static final  textStyle15Black =  TextStyle(
       fontSize: 16,
-      color: Colors.black
+      color: AppColors.black
   );
-  static const textStyle15DeepPurple =  TextStyle(
+  static final textStyle15DeepPurple =  TextStyle(
       fontSize: 16,
-      color: Colors.deepPurple
+      color: AppColors.deepPurple
   );
 
 }
